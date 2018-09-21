2bbc7a3ba0e52029b45a50a293be99493134d220
yongminss <kyongmin96@naver.com>
2018-09-22 01:59:30 +0900

    hw_0918

--- hw_0918/hw_0918.py
+++ hw_0918/hw_0918.py
@@ -1,32 +1,30 @@
 from pico2d import *
 
-def handle_events(speed):
+def handle_events():
     global running
     global x, y
     events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            if x <= event.x:
+    for e in events:
+        if e.type == SDL_QUIT:
+            if e.key == SDLK_ESCAPE:
+                running = False
+        elif e.type == SDL_MOUSEMOTION:
+            if x <= e.x:
                 x += speed
-                if x >= event.x:
-                    x = event.x
-            elif x >= event.x:
+                if x >= e.x:
+                    x = e.x
+            elif x >= e.x:
                 x -= speed
-                if x <= event.x:
-                    x = event.x
-            if y <= 600 - 1 - event.y:
+                if x <= e.x:
+                    x = e.x
+            if y <= 600 - e.y:
                 y += speed
-                if y >= event.y:
-                    y = 600 - 1 - event.y
-            elif y >= 600 - 1 - event.y:
+                if y >= 600 - e.y:
+                    y = 600 - e.y
+            elif y >= 600 - e.y:
                 y -= speed
-                if y <= event.y:
-                    y = 600 - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = false
-    
+                if y <= 600 - e.y:
+                    y = 600 - e.y
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
@@ -35,14 +33,16 @@ character = load_image('run_animation.png')
 x = 0
 y = 90
 frame = 0
+speed = 10
+running = True
 
-while (1):
+while running:
     clear_canvas()
     grass.draw(400, 30)
     character.clip_draw(frame * 100, 0, 100, 100, x, y)
     frame = (frame + 1) % 8
     update_canvas()
-    handle_events(10)
+    handle_events()
     delay(0.01)
     get_events()
     
