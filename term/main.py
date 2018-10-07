5fb9ead836675f18d63042ed5bacec828a5de648
yongminss <kyongmin96@naver.com>
2018-10-07 19:36:58 +0900

    title 화면 추가

--- term/main.py
+++ term/main.py
@@ -1,5 +1,7 @@
 from pico2d import *
+import game_framework
+import title_state
 
 open_canvas()
-
+game_framework.run(title_state)
 close_canvas()
--- term/title_state.py
+++ term/title_state.py
@@ -1,1 +1,41 @@
+from pico2d import *
 import game_framework
+
+class Title:
+    def __init__(self):
+        self.image = load_image('../term/cookierun_image/cookierun_title.png')
+    def draw(self):
+        self.image.draw(0,0)
+
+def handle_events():
+
+    events = get_events()
+
+    for e in events:
+        if e.type == SDL_QUIT:
+            game_framework.quit()
+        elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+            
+def enter():
+
+    global title
+
+    title = Title()
+
+def draw():
+
+    global title
+
+    clear_canvas()
+    title.draw()
+    update_canvas()
+
+def update():
+    pass
+
+def resume():
+    pass
+
+def exit():
+    pass
