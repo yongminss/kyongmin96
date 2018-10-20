5fce6a94c0557f2c4c771cecc93f3e1f6ddd501f
yongminss <kyongmin96@naver.com>
2018-10-20 23:03:54 +0900

    Cookie 1단 점프, 슬라이딩 추가

--- term/cookie_state.py
+++ term/cookie_state.py
@@ -2,22 +2,31 @@ from pico2d import *
 import game_framework
 
 class Cookie:
-    RUN, JUMP, SLIDE = 0, 1, 2
+    RUN, JUMP, DOUBLE_JUMP, SLIDE = 0, 1, 2, 3
     
     def __init__(self):
         self.cookie = load_image('../term/cookierun_image/Cookie_Run_State.png')
-        self.state = self.RUN   # 쿠키의 상태
+        self.state = self.JUMP   # 쿠키의 상태
         self.x = 200    # 쿠키 x좌표
         self.y = 200    # 쿠키 y좌표
         self.frame = 0
 
     def draw(self):
-        self.cookie.clip_draw(self.frame * 120, 382 - 135, 120, 135, self.x, self.y)
-        #self.cookie.clip_draw(0, 382 - 135 - 165, 140, 165, self.x, self.y)
-        #self.cookie.clip_draw(0, 382 - 135 - 165 - 80, 170, 80, self.x, self.y)
+        if self.state == self.RUN:  # 달리기
+            self.cookie.clip_draw(self.frame * 120, 382 - 135, 120, 135,
+                                  self.x, self.y)
+        elif self.state == self.JUMP:   # 1단 점프
+            self.cookie.clip_draw(0, 382 - 135 - 165, 140, 165, self.x, self.y)
+        elif self.state == self.SLIDE:  # 슬라이딩
+            self.cookie.clip_draw(self.frame * 170, 382 - 135 - 165 - 80, 170, 80, self.x, self.y)
 
     def update(self):
-        self.frame = (self.frame + 1) % 3
+        if self.state == self.RUN:
+            self.frame = (self.frame + 1) % 3
+        elif self.state == self.JUMP:
+            self.frame = (self.frame + 1) % 1
+        elif self.state == self.SLIDE:
+            self.frame = (self.frame + 1) % 2
         
     def handle_events(self, e):
         pass
