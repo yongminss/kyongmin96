ad59933ffa14300a585162775d4d6fab4eb4b14d
yongminss <kyongmin96@naver.com>
2018-11-10 16:31:56 +0900

    cookie py 파일 추가

--- term/cookie.py
+++ term/cookie.py
@@ -2,13 +2,6 @@ from pico2d import *
 import game_world
 import time
 
-RUN, JUMP, DOUBLE_JUMP, SLIDE = range(4)
-
-key_event_table = {
-    (SDL_KEYDOWN, SDLK_SPACE): JUMP,
-    (SDL_KEYDOWN, SDLK_DOWN): SLIDE,
-}
-
 class RunState:
     @staticmethod
     def enter(cookie):
@@ -63,9 +56,37 @@ class SlideState:
 
 
 class Cookie:
-    image = None
-
+    RUN, JUMP, DOUBLE_JUMP, SLIDE = range(4)
+    
     def __init__(self):
-        if Cookie.image == None:
-            Cookie.image = load_image('../term/cookierun_image/Cookie_Run_State.png')
-            self.state = self.RUN
+        self.cookie = load_image('../term/cookierun_image/Cookie_Run_State.png')
+        self.state = self.RUN   # 쿠키의 상태
+        self.x = 250    # 쿠키 x좌표
+        self.y = 215    # 쿠키 y좌표
+        self.frame = 0
+
+    def draw(self):
+        if self.state == self.RUN:              # 달리기
+            self.cookie.clip_draw(self.frame * 120, 382 - 135, 120, 135,
+                                  self.x, self.y)
+        elif self.state == self.JUMP:           # 1단 점프
+            self.cookie.clip_draw(0, 382 - 135 - 165, 140, 165, self.x, self.y)
+        elif self.state == self.DOUBLE_JUMP:    # 2단 점프
+            self.cookie.clip_draw(self.frame * 140, 382 - 135 - 165, 140, 165,
+                                  self.x, self.y)
+        elif self.state == self.SLIDE:          # 슬라이딩
+            self.cookie.clip_draw(self.frame * 170, 382 - 135 - 165 - 80, 170, 80,
+                                  self.x, self.y)
+
+    def update(self):
+        if self.state == self.RUN:
+            self.frame = (self.frame + 1) % 3
+        elif self.state == self.JUMP:
+            self.frame = (self.frame + 1) % 1
+        elif self.state == self.DOUBLE_JUMP:
+            self.frame = (self.frame + 1) % 6
+        elif self.state == self.SLIDE:
+            self.frame = (self.frame + 1) % 2
+        
+    def handle_events(self, e):
+        pass
--- term/cookie_state.py
+++ term/cookie_state.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import game_framework
 import game_world
+from cookie import Cookie
 from stage import Stage
 from ground import Ground
 from hp import HP
@@ -9,42 +10,6 @@ from jelly import Jelly
 from trap1 import jTrap
 from trap2 import sTrap
 
-class Cookie:
-    RUN, JUMP, DOUBLE_JUMP, SLIDE = 0, 1, 2, 3
-    
-    def __init__(self):
-        self.cookie = load_image('../term/cookierun_image/Cookie_Run_State.png')
-        self.state = self.RUN   # 쿠키의 상태
-        self.x = 250    # 쿠키 x좌표
-        self.y = 215    # 쿠키 y좌표
-        self.frame = 0
-
-    def draw(self):
-        if self.state == self.RUN:  # 달리기
-            self.cookie.clip_draw(self.frame * 120, 382 - 135, 120, 135,
-                                  self.x, self.y)
-        elif self.state == self.JUMP:   # 1단 점프
-            self.cookie.clip_draw(0, 382 - 135 - 165, 140, 165, self.x, self.y)
-        elif self.state == self.DOUBLE_JUMP:    # 2단 점프
-            self.cookie.clip_draw(self.frame * 140, 382 - 135 - 165, 140, 165,
-                                  self.x, self.y)
-        elif self.state == self.SLIDE:  # 슬라이딩
-            self.cookie.clip_draw(self.frame * 170, 382 - 135 - 165 - 80, 170, 80,
-                                  self.x, self.y)
-
-    def update(self):
-        if self.state == self.RUN:
-            self.frame = (self.frame + 1) % 3
-        elif self.state == self.JUMP:
-            self.frame = (self.frame + 1) % 1
-        elif self.state == self.DOUBLE_JUMP:
-            self.frame = (self.frame + 1) % 6
-        elif self.state == self.SLIDE:
-            self.frame = (self.frame + 1) % 2
-        
-    def handle_events(self, e):
-        pass
-
 def handle_events():
     
     events = get_events()
@@ -52,22 +17,26 @@ def handle_events():
     for e in events:
         if e.type == SDL_QUIT:
              game_framework.quit()
+        else:
+            cookie.handle_events(e)
 
 def enter():
 
-    global stage, ground, hp, potion, jelly
+    global stage, ground, hp, cookie, potion, jelly, jtrap, strap
 
-    stage = Stage()
-    ground = Ground()
-    hp = HP()
-    potion = Potion()
-    jelly = Jelly()
-    jtrap = jTrap()
-    strap = sTrap()
+    stage = Stage()         # 스테이지
+    ground = Ground()       # 바닥
+    hp = HP()               # 체력
+    cookie = Cookie()       # 캐릭터
+    potion = Potion()       # HP포션
+    jelly = Jelly()         # 젤리
+    jtrap = jTrap()         # 점프 함정
+    strap = sTrap()         # 슬라이드 함정
     
     game_world.add_object(stage, game_world.layer_bg)
     game_world.add_object(ground, game_world.layer_bg)
     game_world.add_object(hp, game_world.layer_bg)
+    game_world.add_object(cookie, game_world.layer_player)
     game_world.add_object(potion, game_world.layer_obstacle)
     game_world.add_object(jelly, game_world.layer_obstacle)
     game_world.add_object(jtrap, game_world.layer_obstacle)
