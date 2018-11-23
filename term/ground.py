dd9cae33ccb24f4550fff331f148166aca92aa28
yongminss <kyongmin96@naver.com>
2018-11-23 21:50:29 +0900

    그라운드에 ParallexLayer 적용

--- term/ground.py
+++ term/ground.py
@@ -1,30 +1,35 @@
 from pico2d import *
 
-class Ground:
-    STAGE_1, STAGE_2 = range(2)
-    
-    def __init__(self):
-        self.First_Ground = load_image('../term/cookierun_image/Ground_01.png')
-        self.Second_Ground = load_image('../term/cookierun_image/Ground_02.png')
+class ParallexLayer:
+    def __init__(self, imageName):
+        self.image = load_image(imageName)
+        self.w, self.h = 800, 600
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
         self.frame = 0
-        self.state = self.STAGE_1
 
     def draw(self):
-        if self.state == self.STAGE_1:
-            self.First_Ground.draw(400, 350)
-        elif self.state == self.STAGE_2:
-            self.Second_Ground.draw(400, 350)
+        self.image.clip_draw_to_origin(self.x1, 0, self.w1, self.h, 0, 0)
+        self.image.clip_draw_to_origin(self.x2, 0, self.w2, self.h, self.w1, 0)
+    
+    def update(self):
+        self.frame += 5
+        self.x1 = self.frame % self.image.w
+        self.w1 = self.image.w - self.x1
+        self.x2 = 0
+        self.w2 = self.cw - self.w1
+
+class Ground:
+    def __init__(self):
+        self.Fground = [
+            ParallexLayer('../term/cookierun_image/Ground_01.png')
+            ]
+        self.Sground = [
+            ParallexLayer('../term/cookierun_image/Ground_02.png')
+            ]
+
+    def draw(self):
+        for l in self.Fground: l.draw()
 
     def update(self):
-        if self.state == self.STAGE_1:
-            self.frame += 10
-            if self.frame >= 2490:
-                self.state = self.STAGE_2
-                self.frame = 0
-        elif self.state == self.STAGE_2:
-            self.frame += 10
-            if self.frame >= 2490:
-                self.frame = 0
-        
-    def exit(self):
-        pass
+        for l in self.Fground: l.update()
