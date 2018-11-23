447a22144cd1b960fdf0d191b8a955efadbe3af8
yongminss <kyongmin96@naver.com>
2018-11-23 15:57:05 +0900

    스테이지에 ParallexLayer 적용

--- term/stage.py
+++ term/stage.py
@@ -1,28 +1,35 @@
 from pico2d import *
-import game_framework
 
-class Stage:
-    STAGE_1, STAGE_2 = range(2)
-
-    def __init__(self):
-        self.First_Stage = load_image('../term/cookierun_image/Stage_01.png')
-        self.Second_Stage = load_image('../term/cookierun_image/Stage_02.png')
+class ParallexLayer:
+    def __init__(self, imageName):
+        self.image = load_image(imageName)
+        self.w, self.h = 800, 600
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
         self.frame = 0
-        self.state = self.STAGE_1
 
     def draw(self):
-        if self.state == self.STAGE_1:
-            self.First_Stage.clip_draw(self.frame, 0, 800, 600, 400, 300)
-        elif self.state == self.STAGE_2:
-            self.Second_Stage.clip_draw(self.frame, 0, 800, 600, 400, 300)  
+        self.image.clip_draw_to_origin(self.x1, 0, self.w1, self.h, 0, 0)
+        self.image.clip_draw_to_origin(self.x2, 0, self.w2, self.h, self.w1, 0)
+    
+    def update(self):
+        self.frame += 10
+        self.x1 = self.frame % self.image.w
+        self.w1 = self.image.w - self.x1
+        self.x2 = 0
+        self.w2 = self.cw - self.w1
+
+class Stage:
+    def __init__(self):
+        self.Fstage = [
+            ParallexLayer('../term/cookierun_image/Stage_01.png')
+            ]
+        self.Sstage = [
+            ParallexLayer('../term/cookierun_image/Stage_02.png')
+            ]
+        
+    def draw(self):
+        for l in self.Fstage: l.draw()
 
     def update(self):
-        if self.state == self.STAGE_1:
-            self.frame += 10
-            if self.frame >= 2490:           # 일정 거리에 도착하면
-                self.state = self.STAGE_2    # 스테이지 2로 교체
-                self.frame = 0
-        elif self.state == self.STAGE_2:
-            self.frame += 10
-            if self.frame >= 2490:
-                game_framework.quit()
+        for l in self.Fstage: l.update()
