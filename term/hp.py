cfd307580407afc3e39543e4bc96687f749c576a
yongminss <kyongmin96@naver.com>
2018-12-02 20:45:36 +0900

    젤리 충돌체크 및 등장조건 추가

--- term/hp.py
+++ term/hp.py
@@ -1,36 +1,14 @@
 from pico2d import *
-import time
-import game_world
-from cookie import Cookie
-from potion import Potion
-
-def collides(a, b):
-    if not hasattr(a, 'get_bb'): return False
-    if not hasattr(b, 'get_bb'): return False
-
-    la, ba, ra, ta = a.get_bb()
-    lb, bb, rb, tb = b.get_bb()
-    
-    if la > rb: return False
-    if ra < lb: return False
-    if ta < bb: return False
-    if ba > tb: return False
-    return True
 
 class HP:
     MAX_HP = 500
     def __init__(self):
-        global cookie, potion
         
         self.HP = load_image('../term/cookierun_image/HP.png')
-        self.HP_count = 500
+        self.HP_count = self.MAX_HP
         self.x = 400
         self.y = 500
-        cookie = Cookie()
-        potion = Potion()
-
-        game_world.add_object(potion, game_world.layer_obstacle)
-
+        
     def draw(self):
         self.HP.clip_draw(0, 0, self.HP_count, 64, self.x, self.y)
 
@@ -39,11 +17,6 @@ class HP:
         # 체력 최대치
         if self.HP_count >= self.MAX_HP:
             self.HP_count = self.MAX_HP
-        # 쿠키와 포션의 충돌 시
-        for potion in game_world.objects_at_layer(game_world.layer_obstacle):
-            if collides(cookie, potion):
-                game_world.remove_object(potion)
-                self.HP_count += 100
-                
+                        
     def exit(self):
         pass
--- term/jelly.py
+++ term/jelly.py
@@ -1,4 +1,6 @@
 from pico2d import *
+import random
+import game_world
 import config
 
 class Jelly:
@@ -7,27 +9,19 @@ class Jelly:
     def __init__(self):
         if Jelly.jelly == None:
             self.jelly = load_image('../term/cookierun_image/Item_Jelly.png')
-        self.x = 900  # 젤리 x 좌표
-        self.y = 270  # 젤리 y 좌표
-        self.state = self.OFF
-        self.frame = 0
+        self.x = 850                            # 젤리 x 좌표
+        self.y = 270 + random.randint(0, 200)   # 젤리 y 좌표
 
     def get_bb(self):
         return self.x - 10, self.y - 15, self.x + 10, self.y + 15
 
     def draw(self):
-        if self.state == self.ON:
-            self.jelly.draw(self.x, self.y)
+        self.jelly.draw(self.x, self.y)
         # 충돌체크 박스
         if config.draws_bounding_box:
             draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.frame += 10
-        if self.frame >= 2490:
-                self.frame = 0
-        # 젤리 이벤트
-        if self.frame > 50:
-            self.state = self.ON
-            if self.state == self.ON:
-                self.x -= 10
+        self.x -= 10
+        if self.x <= -10:
+            game_world.remove_object(self)
